b4dc6ed5c19c28c57940a9f8215dcfb5f561178e
Theodore <theodore.bourgeon@gmail.com>
2023-08-02 14:14:52 +0200

    fix: add type export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+export { CreateChatCompletionResponse } from 'openai';
+export {
+  CreateChatCompletionMessages,
+  CreateChatCompletionRequest,
+} from './interfaces/create-chat-completion.interface';
 export * from './chat.module';
 export * from './chat.service';
