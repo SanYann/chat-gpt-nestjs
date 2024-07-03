b248207e13244e2f08f15f26c2728ff6976ab879
SanYann <yann.aleman27@gmail.com>
2024-07-03 21:25:40 +0200

    feat: remove ressources from openai

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as openaiRessources from 'openai/resources';
+
 export * from './chat.module';
 export * from './chat.service';
 export { CreateChatCompletionRequest } from './interfaces/create-chat-completion.interface';
+export { openaiRessources };
