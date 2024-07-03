15822065371e91ef953de95ee710466cf0d23b2f
SanYann <yann.aleman27@gmail.com>
2024-07-03 21:11:20 +0200

    feat: export ressources from openai

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from 'openai/resources';
 export * from './chat.module';
 export * from './chat.service';
 export { CreateChatCompletionRequest } from './interfaces/create-chat-completion.interface';
